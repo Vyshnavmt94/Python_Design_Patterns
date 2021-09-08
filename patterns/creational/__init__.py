0f3d4b0384da3eb74e01fe593189f887a8cc35e4
Vyshnavmt94 <vyshnav94.mec@gmail.com>
2021-09-08 22:11:08 +0530

    Added factory method

---
+++ patterns/creational/__init__.py
@@ -0,0 +1,4 @@
+# Creational patterns provides essential information regarding the Class instantiation or the object instantiation.
+# Class Creational Pattern and the Object Creational pattern is the major categorization of the Creational Design Patterns.
+# While class-creation patterns use inheritance effectively in the instantiation process,
+# object-creation patterns use delegation effectively to get the job done.
---
+++ patterns/creational/factory/__init__.py
@@ -0,0 +1,2 @@
+# Factory Method is a Creational Design Pattern that allows an interface or a class to create an object, but lets subclasses decide which class or object to instantiate.
+# Here, objects are created without exposing the logic to the client, and for creating the new type of object, the client uses the same common interface.

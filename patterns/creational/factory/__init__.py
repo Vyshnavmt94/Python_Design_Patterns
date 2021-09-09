1298878017ce4286dba512d772ee398538e0eb8e
Vyshnavmt94 <vyshnav94.mec@gmail.com>
2021-09-09 20:47:20 +0530

    Added factory method

--- patterns/creational/factory/__init__.py
+++ patterns/creational/factory/__init__.py
@@ -1,2 +1,10 @@
+
+# ========================================= Factory Method ==========================================
 # Factory Method is a Creational Design Pattern that allows an interface or a class to create an object, but lets subclasses decide which class or object to instantiate.
 # Here, objects are created without exposing the logic to the client, and for creating the new type of object, the client uses the same common interface.
+
+
+# ========================================= AbstractFactory Method ==========================================
+# Allows you to produce the families of related objects without specifying their concrete classes.
+# It provides a way to encapsulate a group of individual factories.
+# Basically, here we try to abstract the creation of the objects depending on the logic, business, platform choice, etc.
